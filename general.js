e769ac2a8fe35cf020ef9730253273b4e29903a0
Diák <petrik@gmail.com>
2026-01-28 12:14:24 +0100

    nevek

---
+++ general.js
@@ -0,0 +1,13 @@
+
+
+const vezeteknevek = [
+  "Nagy", "Kovács", "Tóth", "Szabó", "Horváth",
+  "Varga", "Kiss", "Molnár", "Németh", "Farkas"
+];
+
+const keresztnevek = [
+  "Péter", "Anna", "László", "Éva", "Gábor",
+  "Zsófia", "Tamás", "Judit", "Balázs", "Katalin"
+];
+
+
