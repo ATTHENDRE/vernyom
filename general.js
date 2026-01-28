54b406e8991caf841d7f60fc7d4b4e1ea7df9d3f
Diák <petrik@gmail.com>
2026-01-28 12:20:57 +0100

    Rndom ertkek

--- general.js
+++ general.js
@@ -1,5 +1,6 @@
 
 
+
 const vezeteknevek = [
   "Nagy", "Kovács", "Tóth", "Szabó", "Horváth",
   "Varga", "Kiss", "Molnár", "Németh", "Farkas"
@@ -9,5 +10,16 @@ const keresztnevek = [
   "Péter", "Anna", "László", "Éva", "Gábor",
   "Zsófia", "Tamás", "Judit", "Balázs", "Katalin"
 ];
+
+function randomElem(tomb) {
+  return tomb[Math.floor(Math.random() * tomb.length)];
+}
+
+function randomVernyomas() {
+  const szisztoles = Math.floor(Math.random() * (160 - 90 + 1)) + 90;
+  const diasztoles = Math.floor(Math.random() * (100 - 60 + 1)) + 60;
+  return `${szisztoles}/${diasztoles}`;
+}
+
 
 
